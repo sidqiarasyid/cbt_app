7e15d8fa90e50a5f95916b8808b8b46d6c22c922
gmii <gumilarharisubagja28@gmail.com>
2025-11-05 20:42:12 +0700

    feat: enhance quiz end page with submission timestamp and navigation improvements

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -33,38 +33,45 @@ class _QuizPageState extends State<QuizPage> {
     rAnswer = qList[currentQuestion].rightAnswer;
     answer = qList[currentQuestion].answers;
   }
-  
-  void nextQuestion(){
+
+  void nextQuestion() {
     List<QuizModel> qList = widget.ujian.quizList;
     qList[currentQuestion].isFinished = true;
-    if(currentQuestion + 1 >= qList.length){
-        Navigator.push(context, 
-          MaterialPageRoute(builder: (context) => QuizEndPage()
-          )
-        );
+    if (currentQuestion + 1 >= qList.length) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => QuizEndPage(ujian: widget.ujian),
+        ),
+      );
     } else {
       currentQuestion++;
       setState(() {
         loadCurrentQuestion();
       });
     }
-    
   }
 
-  Future<void> navigatePicker(BuildContext context, List<QuizModel> qList, int curItem) async{
-  int? res;
-  res = await Navigator.push(
-    context, 
-    MaterialPageRoute(builder: (context) => QuizPicker(quizList: qList, currItem: curItem,)));
-   
-   if(!context.mounted) return;
-   res ??= curItem;
-   setState(() {
-     currentQuestion = res!;
-     loadCurrentQuestion();
-   });
-  } 
-  
+  Future<void> navigatePicker(
+    BuildContext context,
+    List<QuizModel> qList,
+    int curItem,
+  ) async {
+    int? res;
+    res = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => QuizPicker(quizList: qList, currItem: curItem),
+      ),
+    );
+
+    if (!context.mounted) return;
+    res ??= curItem;
+    setState(() {
+      currentQuestion = res!;
+      loadCurrentQuestion();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,20 +89,27 @@ class _QuizPageState extends State<QuizPage> {
                     children: [
                       IconButton(
                         onPressed: () {
-                          endQuiz(context, 
-                            (){
+                          endQuiz(
+                            context,
+                            () {
                               Navigator.pop(context);
                               Navigator.pop(context);
-                            }, 
-                            (){
+                            },
+                            () {
                               Navigator.pop(context);
-                            }
+                            },
                           );
-                        }, 
+                        },
                         icon: Icon(Icons.arrow_back),
                         iconSize: 30,
                       ),
-                      Text("Soal ${currentQuestion + 1}", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+                      Text(
+                        "Soal ${currentQuestion + 1}",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ],
                   ),
                   Row(
@@ -103,41 +117,60 @@ class _QuizPageState extends State<QuizPage> {
                       Container(
                         width: 80,
                         alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: 4,
+                        ),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(6),
-                          border: Border.all(
-                            width: 1,
-                            color: Colors.black
-                          )
+                          border: Border.all(width: 1, color: Colors.black),
                         ),
-                        child: Text("40:00", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),),
+                        child: Text(
+                          "40:00",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
                       ),
-                      IconButton(onPressed: (){
-                        navigatePicker(context, widget.ujian.quizList, currentQuestion);
-                      }, 
-                      icon: Icon(Icons.grid_view_outlined, size: 30,))
+                      IconButton(
+                        onPressed: () {
+                          navigatePicker(
+                            context,
+                            widget.ujian.quizList,
+                            currentQuestion,
+                          );
+                        },
+                        icon: Icon(Icons.grid_view_outlined, size: 30),
+                      ),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
-            QuizPilganPage(question: ques, rightAnswer: rAnswer, answerList: answer,),
+            QuizPilganPage(
+              question: ques,
+              rightAnswer: rAnswer,
+              answerList: answer,
+            ),
             Container(
               margin: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
               width: double.infinity,
               height: 50,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(8)
+                    borderRadius: BorderRadiusGeometry.circular(8),
                   ),
-                  backgroundColor: ColorsApp.primaryColor
+                  backgroundColor: ColorsApp.primaryColor,
                 ),
-                onPressed: nextQuestion, 
-                child: Text("Selanjutnya", style: TextStyle(color: ColorsApp.secondaryColor),)),
-            )
+                onPressed: nextQuestion,
+                child: Text(
+                  "Selanjutnya",
+                  style: TextStyle(color: ColorsApp.secondaryColor),
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -145,10 +178,11 @@ class _QuizPageState extends State<QuizPage> {
   }
 }
 
-endQuiz(BuildContext context, VoidCallback yes, VoidCallback no){
-  showDialog(context: context, builder: (context) {
-    return EndQuizDialog(onYesPressed: yes, onNoPressed: no);
-  },
+endQuiz(BuildContext context, VoidCallback yes, VoidCallback no) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return EndQuizDialog(onYesPressed: yes, onNoPressed: no);
+    },
   );
 }
-
