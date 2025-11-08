f74cf82f8575006a56f0ce786a14098893fd59ca
RizzQ <r.ahmadhende10@gmail.com>
2025-11-08 09:18:36 +0700

    Fix Profile page.

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../style/style.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -26,9 +28,9 @@ class _ProfilePageState extends State<ProfilePage> {
     });
   }
 
+  // confirm dialog
   Future<void> _saveChanges() async {
     if (_formKey.currentState!.validate()) {
-      // confirmation dialog
       final bool? confirm = await showDialog<bool>(
         context: context,
         builder: (BuildContext context) {
@@ -76,93 +78,104 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: const Text('Profile'),
-        actions: [
-          IconButton(
-            icon: Icon(_isEditing ? Icons.save : Icons.edit),
-            onPressed: _isEditing ? _saveChanges : _toggleEdit,
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const SizedBox(height: 24),
-                const Image(
-                  image: AssetImage('assets/images/profile.png'),
-                  height: 120,
-                ),
-                const SizedBox(height: 75),
-                if (_isEditing) ...[
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Nama Lengkap',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Nama tidak boleh kosong';
-                      }
-                      return null;
-                    },
+      backgroundColor: ColorsApp.backgroundColor,
+      // appBar: AppBar(
+      //   backgroundColor: ColorsApp.backgroundColor,
+      //   title: const Text(
+      //     'Profile',
+      //     style: TextStyle(
+      //       color: Colors.black,
+      //       fontSize: 20,
+      //       fontWeight: FontWeight.bold,
+      //     ),
+      //   ),
+      // actions: [
+      //   IconButton(
+      //     icon: Icon(_isEditing ? Icons.save : Icons.edit),
+      //     onPressed: _isEditing ? _saveChanges : _toggleEdit,
+      //   ),
+      // ],
+      // automaticallyImplyLeading: false,
+      // ),
+      body: Center(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(35.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  const SizedBox(height: 24),
+                  const Image(
+                    image: AssetImage('assets/images/profile.png'),
+                    height: 150,
                   ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _roleController,
-                    decoration: const InputDecoration(
-                      labelText: 'Role',
-                      border: OutlineInputBorder(),
+                  const SizedBox(height: 75),
+                  if (_isEditing) ...[
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Nama Lengkap',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Nama tidak boleh kosong';
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Role tidak boleh kosong';
-                      }
-                      return null;
-                    },
-                  ),
-                ] else ...[
-                  ListTile(
-                    title: const Text(
-                      'Nama Lengkap',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _roleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Role',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Role tidak boleh kosong';
+                        }
+                        return null;
+                      },
+                    ),
+                  ] else ...[
+                    ListTile(
+                      title: const Text(
+                        'Nama Lengkap',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      subtitle: Text(
+                        _nameController.text,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ),
-                    subtitle: Text(
-                      _nameController.text,
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
+                    const SizedBox(height: 20),
+                    ListTile(
+                      title: const Text(
+                        'Role',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      subtitle: Text(
+                        _roleController.text,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  ListTile(
-                    title: const Text(
-                      'Role',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    subtitle: Text(
-                      _roleController.text,
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
+                  ],
                 ],
-              ],
+              ),
             ),
           ),
         ),
